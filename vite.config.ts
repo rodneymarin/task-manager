45fef443dc2e51d068d469a2f9f7eef505e7ab75
Rodney <rodney.marin@gmail.com>
2025-01-18 19:05:49 -0400

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://dev.rodneymarin.com/task-manager/",
+  base: "https://dev.rodneymarin.com/taskmanager-build/",
   build: {
     outDir: '../build'
   }
